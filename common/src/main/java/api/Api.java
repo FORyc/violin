f44d4bcd8f4cfdc7991ed567429a67224873be22
FORyc <952717308@qq.com>
2020-01-05 23:02:20 +0800

    父模块pom文件取消maven插件已保证打包的时候不检查启动类 公共模块的Api类修改对通用返回的状态码依赖

--- common/src/main/java/api/Api.java
+++ common/src/main/java/api/Api.java
@@ -2,6 +2,7 @@ package api;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import constants.Constant;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,25 +15,23 @@ public class Api {
     /**
      * 通用成功返回
      */
-    public static final int SUCCESS_CODE = 200000;
     public static final String SUCCESS_MESSAGE = "success";
 
     /**
      * 通用失败返回
      */
-    public static final int FAIL_CODE = 500000;
     public static final String FAIL_MESSAGE = "fail";
 
     public static String success() {
         Map<String, Object> map = new HashMap<>();
-        map.put("status", SUCCESS_CODE);
+        map.put("status", Constant.SUCCESS);
         map.put("message", SUCCESS_MESSAGE);
         return JSONObject.toJSONString(map);
     }
 
     public static String success(int status, Object data) {
         Map<String, Object> map = new HashMap<>();
-        map.put("status", SUCCESS_CODE);
+        map.put("status", status);
         map.put("message", SUCCESS_MESSAGE);
         map.put("data", data);
         return JSONObject.toJSONString(map, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullNumberAsZero, SerializerFeature.WriteNullListAsEmpty, SerializerFeature.WriteNullBooleanAsFalse);
@@ -40,18 +39,18 @@ public class Api {
 
     public static String success(String message) {
         Map<String, Object> map = new HashMap<>();
-        map.put("status", SUCCESS_CODE);
+        map.put("status", Constant.SUCCESS);
         map.put("message", message);
         return JSONObject.toJSONString(map);
     }
 
     public static String success(Object data) {
-        return success(SUCCESS_CODE, data);
+        return success(Constant.SUCCESS, data);
     }
 
     public static String error() {
         Map<String, Object> map = new HashMap<>();
-        map.put("status", FAIL_CODE);
+        map.put("status", Constant.FAIL);
         map.put("message", FAIL_MESSAGE);
         return JSONObject.toJSONString(map);
     }
@@ -64,7 +63,7 @@ public class Api {
     }
 
     public static String error(String message) {
-        return error(FAIL_CODE, message);
+        return error(Constant.FAIL, message);
     }
 
 
