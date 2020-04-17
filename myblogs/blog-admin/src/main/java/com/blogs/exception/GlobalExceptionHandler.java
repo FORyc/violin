157fced212d0de2e5749956521ccd6da898cb345
FORyc <952717308@qq.com>
2020-04-17 22:07:00 +0800

    全局异常处理新增日志输出

--- myblogs/blog-admin/src/main/java/com/blogs/exception/GlobalExceptionHandler.java
+++ myblogs/blog-admin/src/main/java/com/blogs/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.blogs.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +15,17 @@ import top.exception.BusinessException;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public <T> CommonResult<T> handle(Exception e){
         if(e instanceof BusinessException){
+            log.warn("程序出现异常", e);
             BusinessException businessException = (BusinessException) e;
             return CommonResult.error(businessException.getErrorCode(), businessException.getErrorMessage());
         }
+        log.warn("程序出现异常", e);
         return CommonResult.error(e.getMessage());
     }
 }
