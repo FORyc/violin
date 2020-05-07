49d258b9a2791ffbabfc70981af5ad8e3aee3dd1
FORyc <952717308@qq.com>
2020-05-07 22:24:56 +0800

    修改swagger的扫描路径及基础配置

--- myblogs/blog-admin/src/main/java/com/blogs/configuration/swagger/Swagger2Config.java
+++ myblogs/blog-admin/src/main/java/com/blogs/configuration/swagger/Swagger2Config.java
@@ -18,10 +18,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
-    private static final String BASE_PACKAGE_URL = "";
-    private static final String TITLE = "";
-    private static final String DESCRIPTION = "";
-    private static final String VERSION = "";
+    private static final String BASE_PACKAGE_URL = "com.blogs.xuan";
+    private static final String TITLE = "TIANXUAN.TOP";
+    private static final String DESCRIPTION = "blog";
+    private static final String VERSION = "1.0";
 
     @Bean
     public Docket createDocket() {
@@ -33,7 +33,7 @@ public class Swagger2Config {
                 .forCodeGeneration(true)
                 .useDefaultResponseMessages(true)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE_URL + ""))
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE_URL + ".controller"))
                 .paths(PathSelectors.any())
                 .build();
 
